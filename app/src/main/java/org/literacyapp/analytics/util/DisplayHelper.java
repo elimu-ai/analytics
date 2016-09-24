09517b59cab9e9003fc374262bcdd58df1faf21f
Jo Grimstad <jo@literacyapp.org>
2016-09-24 16:41:02 +0300

    #8 Reduce image size
    Reduce max image size of captured screenshot to 320px

--- app/src/main/java/org/literacyapp/analytics/util/DisplayHelper.java
+++ app/src/main/java/org/literacyapp/analytics/util/DisplayHelper.java
@@ -1,10 +1,18 @@
 package org.literacyapp.analytics.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.text.format.DateFormat;
 import android.util.Log;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class DisplayHelper {
@@ -15,7 +23,8 @@ public class DisplayHelper {
     public static File captureScreenshot() {
         Log.i(DisplayHelper.class.getName(), "captureScreenshot");
 
-        String screenshotsPath = Environment.getExternalStorageDirectory() + File.separator + ".literacyapp-analytics" + File.separator + "screenshots";
+//        String screenshotsPath = Environment.getExternalStorageDirectory() + File.separator + ".literacyapp-analytics" + File.separator + "screenshots";
+        String screenshotsPath = Environment.getExternalStorageDirectory() + File.separator + "literacyapp-analytics" + File.separator + "screenshots";
         File screenshotsDir = new File(screenshotsPath);
         if (!screenshotsDir.exists()) {
             screenshotsDir.mkdirs();
@@ -31,5 +40,42 @@ public class DisplayHelper {
         });
         Log.i(DisplayHelper.class.getName(), "isScreencapSuccess: " + isScreencapSuccess);
         return screenshotFile;
+    }
+
+    public static File resizeBitmap(File screenshotFile, int maxImageSize, boolean filter) {
+        Log.i(DisplayHelper.class.getName(), "resizeBitmap");
+
+        Bitmap bitmapOriginal = BitmapFactory.decodeFile(screenshotFile.getAbsolutePath());
+        Log.i(DisplayHelper.class.getName(), "bitmapOriginal: " + bitmapOriginal.getWidth() + "px x " + bitmapOriginal.getHeight() + "px");
+
+        float ratio = Math.min(
+                (float) maxImageSize / bitmapOriginal.getWidth(),
+                (float) maxImageSize / bitmapOriginal.getHeight()
+        );
+        int width = Math.round(ratio * bitmapOriginal.getWidth());
+        int height = Math.round(ratio * bitmapOriginal.getHeight());
+
+        Bitmap bitmapScaled = Bitmap.createScaledBitmap(bitmapOriginal, width, height, filter);
+        Log.i(DisplayHelper.class.getName(), "bitmapScaled: " + bitmapScaled.getWidth() + "px x " + bitmapScaled.getHeight() + "px");
+
+        // Save scaled bitmap as file
+        String scaledScreenshotPath = screenshotFile.getAbsolutePath();
+        scaledScreenshotPath = scaledScreenshotPath.replace(".png", "_" + maxImageSize + ".png");
+        Log.i(DisplayHelper.class.getName(), "scaledScreenshotPath: " + scaledScreenshotPath);
+        File scaledScreenshotFile = new File(scaledScreenshotPath);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmapScaled.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        Log.i(DisplayHelper.class.getName(), "bytes.length: " + bytes.length);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(scaledScreenshotFile);
+            IOUtils.write(bytes, fileOutputStream);
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            Log.e(DisplayHelper.class.getName(), null, e);
+        } catch (IOException e) {
+            Log.e(DisplayHelper.class.getName(), null, e);
+        }
+        return scaledScreenshotFile;
     }
 }
