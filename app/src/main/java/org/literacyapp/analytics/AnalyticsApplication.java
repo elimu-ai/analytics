9592f87071b7a0757b5434517524fffcbcf7a60f
Jo Grimstad <jo@literacyapp.org>
2016-11-27 13:46:31 +0300

    Add versionCode upgrade detection

--- app/src/main/java/org/literacyapp/analytics/AnalyticsApplication.java
+++ app/src/main/java/org/literacyapp/analytics/AnalyticsApplication.java
@@ -1,13 +1,18 @@
 package org.literacyapp.analytics;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import org.greenrobot.greendao.database.Database;
 import org.literacyapp.analytics.dao.CustomDaoMaster;
 import org.literacyapp.analytics.dao.DaoSession;
+import org.literacyapp.analytics.util.VersionHelper;
 
 public class AnalyticsApplication extends Application {
+
+    public static final String PREF_APP_VERSION_CODE = "pref_last_content_sync";
 
     private DaoSession daoSession;
 
@@ -19,6 +24,18 @@ public class AnalyticsApplication extends Application {
         CustomDaoMaster.DevOpenHelper helper = new CustomDaoMaster.DevOpenHelper(this, "analytics-db");
         Database db = helper.getWritableDb();
         daoSession = new CustomDaoMaster(db).newSession();
+
+        // Check if the application's versionCode was upgraded
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        int oldVersionCode = sharedPreferences.getInt(PREF_APP_VERSION_CODE, 0);
+        int newVersionCode = VersionHelper.getAppVersionCode(getApplicationContext());
+        if ((oldVersionCode > 0) && (oldVersionCode < newVersionCode)) {
+            Log.i(getClass().getName(), "Upgrading application from version " + oldVersionCode + " to " + newVersionCode);
+//            if (newVersionCode == ???) {
+//                // Put relevant tasks required for upgrading here
+//            }
+            sharedPreferences.edit().putInt(PREF_APP_VERSION_CODE, newVersionCode).commit();
+        }
     }
 
     public DaoSession getDaoSession() {
