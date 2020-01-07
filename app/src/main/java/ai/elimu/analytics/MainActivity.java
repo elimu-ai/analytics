665d0754a423392c16fb7bdd394dee75eb43c387
Nya Elimu <nya@elimu.ai>
2020-01-07 18:08:14 +0800

    #51 Add StoryBookLearningEvent
    - If the database is not empty, redirect the user to the EventListActivity

--- app/src/main/java/ai/elimu/analytics/MainActivity.java
+++ app/src/main/java/ai/elimu/analytics/MainActivity.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.List;
+
+import ai.elimu.analytics.dao.StoryBookLearningEventDao;
+import ai.elimu.analytics.db.AnalyticsRoomDatabase;
+import ai.elimu.analytics.entity.StoryBookLearningEvent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(getApplicationContext(), EventListActivity.class));
-        finish();
+        // If the database is not empty, redirect the user to the EventListActivity
+        AnalyticsRoomDatabase analyticsRoomDatabase = AnalyticsRoomDatabase.getDatabase(getApplicationContext());
+        StoryBookLearningEventDao storyBookLearningEventDao = analyticsRoomDatabase.storyBookLearningEventDao();
+        AnalyticsRoomDatabase.databaseWriteExecutor.execute(() -> {
+            List<StoryBookLearningEvent> storyBookLearningEvents = storyBookLearningEventDao.loadAll();
+            Log.d(getClass().getName(), "storyBookLearningEvents.size(): " + storyBookLearningEvents.size());
+            if (!storyBookLearningEvents.isEmpty()) {
+                startActivity(new Intent(getApplicationContext(), EventListActivity.class));
+                finish();
+            }
+        });
     }
 }
