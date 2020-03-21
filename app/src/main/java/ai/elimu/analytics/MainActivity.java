8d6c8f14db691d064d8a19a347aa30ad0766c2be
Nya Elimu <nya@elimu.ai>
2020-03-21 19:05:12 +0800

    #73 Export events to CSV files
    - Added ExportEventsToCsvWorker

--- app/src/main/java/ai/elimu/analytics/MainActivity.java
+++ app/src/main/java/ai/elimu/analytics/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 import ai.elimu.analytics.dao.StoryBookLearningEventDao;
 import ai.elimu.analytics.db.RoomDb;
 import ai.elimu.analytics.entity.StoryBookLearningEvent;
+import ai.elimu.analytics.task.ExportEventsToCsvWorker;
 import ai.elimu.analytics.task.UploadEventsWorker;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,17 +46,26 @@ public class MainActivity extends AppCompatActivity {
         Log.i(getClass().getName(), "onStart");
         super.onStart();
 
-        // Initiate periodic upload of events to the server
-        Constraints constraints = new Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build();
-        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(UploadEventsWorker.class, 24, TimeUnit.HOURS)
-                .setConstraints(constraints)
+        // Periodically export events to CSV files
+        PeriodicWorkRequest exportEventsToCsvWorkRequest = new PeriodicWorkRequest.Builder(ExportEventsToCsvWorker.class, 1, TimeUnit.HOURS)
                 .build();
         WorkManager.getInstance(getApplicationContext()).enqueueUniquePeriodicWork(
-                "upload_events_worker",
+                "export_events_to_csv",
                 ExistingPeriodicWorkPolicy.KEEP,
-                periodicWorkRequest
+                exportEventsToCsvWorkRequest
+        );
+
+        // Periodically upload events (CSV files) to the server
+        Constraints uploadEventsConstraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        PeriodicWorkRequest uploadEventsWorkRequest = new PeriodicWorkRequest.Builder(UploadEventsWorker.class, 12, TimeUnit.HOURS)
+                .setConstraints(uploadEventsConstraints)
+                .build();
+        WorkManager.getInstance(getApplicationContext()).enqueueUniquePeriodicWork(
+                "upload_events",
+                ExistingPeriodicWorkPolicy.KEEP,
+                uploadEventsWorkRequest
         );
     }
 }
---
+++ app/src/main/java/ai/elimu/analytics/task/ExportEventsToCsvWorker.java
@@ -0,0 +1,99 @@
+package ai.elimu.analytics.task;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import ai.elimu.analytics.dao.StoryBookLearningEventDao;
+import ai.elimu.analytics.db.RoomDb;
+import ai.elimu.analytics.entity.StoryBookLearningEvent;
+
+public class ExportEventsToCsvWorker extends Worker {
+
+    public ExportEventsToCsvWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i(getClass().getName(), "doWork");
+
+        // Extract StoryBookLearningEvents from the database that have not yet been exported to CSV.
+        RoomDb roomDb = RoomDb.getDatabase(getApplicationContext());
+        StoryBookLearningEventDao storyBookLearningEventDao = roomDb.storyBookLearningEventDao();
+        List<StoryBookLearningEvent> storyBookLearningEvents = storyBookLearningEventDao.loadAll();
+        Log.i(getClass().getName(), "storyBookLearningEvents.size(): " + storyBookLearningEvents.size());
+
+        CSVFormat csvFormat = CSVFormat.DEFAULT
+                .withHeader(
+                        "id",
+                        "androidId",
+                        "packageName",
+                        "time",
+                        "storyBookId",
+                        "learningEventType"
+                );
+        StringWriter stringWriter = new StringWriter();
+        try {
+            CSVPrinter csvPrinter = new CSVPrinter(stringWriter, csvFormat);
+
+            // Generate one CSV file per day of events
+            String dateOfPreviousEvent = null;
+            for (StoryBookLearningEvent storyBookLearningEvent : storyBookLearningEvents) {
+                // Export event to CSV file. Example format: "files/storybook-learning-events/storybook-learning-events_2020-03-21.csv"
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String date = simpleDateFormat.format(storyBookLearningEvent.getTimestamp().getTime());
+                if (!date.equals(dateOfPreviousEvent)) {
+                    // Reset file content
+                    stringWriter = new StringWriter();
+                    csvPrinter = new CSVPrinter(stringWriter, csvFormat);
+                }
+                dateOfPreviousEvent = date;
+                String csvFilename = "storybook-learning-events_" + date + ".csv";
+                Log.i(getClass().getName(), "csvFilename: " + csvFilename);
+
+                // TODO: convert to StoryBookLearningEventGson
+
+                csvPrinter.printRecord(
+                        storyBookLearningEvent.getId(),
+                        storyBookLearningEvent.getAndroidId(),
+                        storyBookLearningEvent.getPackageName(),
+                        storyBookLearningEvent.getTimestamp().getTimeInMillis(),
+                        storyBookLearningEvent.getStoryBookId(),
+                        storyBookLearningEvent.getLearningEventType()
+                );
+                csvPrinter.flush();
+
+                String csvFileContent = stringWriter.toString();
+                Log.i(getClass().getName(), "csvFileContent.length(): " + csvFileContent.length());
+                Log.i(getClass().getName(), "csvFileContent: \n" + csvFileContent);
+
+                // Write the content to the CSV file
+                File filesDir = getApplicationContext().getFilesDir();
+                File storyBookLearningEventsDir = new File(filesDir, "storybook-learning-events");
+                File csvFile = new File(storyBookLearningEventsDir, csvFilename);
+                Log.i(getClass().getName(), "csvFile: " + csvFile);
+                FileUtils.writeStringToFile(csvFile, csvFileContent, "UTF-8");
+            }
+        } catch (IOException e) {
+            Log.e(getClass().getName(), null, e);
+        }
+
+        return Result.success();
+    }
+}
