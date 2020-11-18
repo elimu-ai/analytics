aeb8303f72307997a85bea70631eee04db1cb849
Nya <info@elimu.ai>
2020-11-18 12:50:31 +0100

    #96 Added missing methods to LearningEventUtil and AssessmentEventUtil

--- utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.java
+++ utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.java
@@ -2,16 +2,24 @@ package ai.elimu.analytics.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
-import ai.elimu.model.enums.analytics.LearningEventType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import ai.elimu.analytics.utils.converter.CursorToWordAssessmentEventGsonConverter;
+import ai.elimu.model.v2.gson.analytics.WordAssessmentEventGson;
 import ai.elimu.model.v2.gson.content.LetterGson;
 import ai.elimu.model.v2.gson.content.WordGson;
 
 public class AssessmentEventUtil {
 
     public static void reportLetterAssessmentEvent(LetterGson letterGson, Float masteryScore, Long timeSpentMs, Context context, String analyticsApplicationId) {
-        Log.i(LearningEventType.class.getName(), "reportLetterAssessmentEvent");
+        Log.i(AssessmentEventUtil.class.getName(), "reportLetterAssessmentEvent");
 
         Intent broadcastIntent = new Intent();
         broadcastIntent.setAction("ai.elimu.intent.action.WORD_ASSESSMENT_EVENT");
@@ -25,7 +33,7 @@ public class AssessmentEventUtil {
     }
 
     public static void reportWordAssessmentEvent(WordGson wordGson, Float masteryScore, Long timeSpentMs, Context context, String analyticsApplicationId) {
-        Log.i(LearningEventType.class.getName(), "reportWordAssessmentEvent");
+        Log.i(AssessmentEventUtil.class.getName(), "reportWordAssessmentEvent");
 
         Intent broadcastIntent = new Intent();
         broadcastIntent.setAction("ai.elimu.intent.action.WORD_ASSESSMENT_EVENT");
@@ -36,5 +44,43 @@ public class AssessmentEventUtil {
         broadcastIntent.putExtra("timeSpentMs", timeSpentMs);
         broadcastIntent.setPackage(analyticsApplicationId);
         context.sendBroadcast(broadcastIntent);
+    }
+
+    public static List<WordAssessmentEventGson> getWordAssessmentEventGsons(Set<Long> idsOfWordsInWordLearningEvents, Context context, String analyticsApplicationId) {
+        Log.i(AssessmentEventUtil.class.getName(), "getWordAssessmentEventGsons");
+
+        List<WordAssessmentEventGson> wordAssessmentEventGsons = new ArrayList<>();
+
+        Uri wordAssessmentEventsUri = Uri.parse("content://" + analyticsApplicationId + ".provider.word_assessment_event_provider/events");
+        Log.i(AssessmentEventUtil.class.getName(), "wordAssessmentEventsUri: " + wordAssessmentEventsUri);
+        Cursor wordAssessmentEventsCursor = context.getContentResolver().query(wordAssessmentEventsUri, null, null, null, null);
+        Log.i(AssessmentEventUtil.class.getName(), "wordAssessmentEventsCursor: " + wordAssessmentEventsCursor);
+        if (wordAssessmentEventsCursor == null) {
+            Log.e(AssessmentEventUtil.class.getName(), "wordAssessmentEventsCursor == null");
+            Toast.makeText(context, "wordAssessmentEventsCursor == null", Toast.LENGTH_LONG).show();
+        } else {
+            Log.i(AssessmentEventUtil.class.getName(), "wordAssessmentEventsCursor.getCount(): " + wordAssessmentEventsCursor.getCount());
+            if (wordAssessmentEventsCursor.getCount() == 0) {
+                Log.e(AssessmentEventUtil.class.getName(), "wordAssessmentEventsCursor.getCount() == 0");
+            } else {
+                boolean isLast = false;
+                while (!isLast) {
+                    wordAssessmentEventsCursor.moveToNext();
+
+                    // Convert from Room to Gson
+                    WordAssessmentEventGson wordAssessmentEventGson = CursorToWordAssessmentEventGsonConverter.getWordAssessmentEventGson(wordAssessmentEventsCursor);
+
+                    wordAssessmentEventGsons.add(wordAssessmentEventGson);
+
+                    isLast = wordAssessmentEventsCursor.isLast();
+                }
+
+                wordAssessmentEventsCursor.close();
+                Log.i(AssessmentEventUtil.class.getName(), "wordAssessmentEventsCursor.isClosed(): " + wordAssessmentEventsCursor.isClosed());
+            }
+        }
+        Log.i(AssessmentEventUtil.class.getName(), "wordAssessmentEventGsons.size(): " + wordAssessmentEventGsons.size());
+
+        return wordAssessmentEventGsons;
     }
 }
--- utils/src/main/java/ai/elimu/analytics/utils/LearningEventUtil.java
+++ utils/src/main/java/ai/elimu/analytics/utils/LearningEventUtil.java
@@ -2,9 +2,19 @@ package ai.elimu.analytics.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ai.elimu.analytics.utils.converter.CursorToWordLearningEventGsonConverter;
 import ai.elimu.model.enums.analytics.LearningEventType;
+import ai.elimu.model.v2.gson.analytics.WordLearningEventGson;
 import ai.elimu.model.v2.gson.content.LetterGson;
 import ai.elimu.model.v2.gson.content.StoryBookGson;
 import ai.elimu.model.v2.gson.content.WordGson;
@@ -12,7 +22,7 @@ import ai.elimu.model.v2.gson.content.WordGson;
 public class LearningEventUtil {
 
     public static void reportLetterLearningEvent(LetterGson letterGson, LearningEventType learningEventType, Context context, String analyticsApplicationId) {
-        Log.i(LearningEventType.class.getName(), "reportLetterLearningEvent");
+        Log.i(LearningEventUtil.class.getName(), "reportLetterLearningEvent");
 
         Intent broadcastIntent = new Intent();
         broadcastIntent.setAction("ai.elimu.intent.action.LETTER_LEARNING_EVENT");
@@ -25,7 +35,7 @@ public class LearningEventUtil {
     }
 
     public static void reportWordLearningEvent(WordGson wordGson, LearningEventType learningEventType, Context context, String analyticsApplicationId) {
-        Log.i(LearningEventType.class.getName(), "reportWordLearningEvent");
+        Log.i(LearningEventUtil.class.getName(), "reportWordLearningEvent");
 
         Intent broadcastIntent = new Intent();
         broadcastIntent.setAction("ai.elimu.intent.action.WORD_LEARNING_EVENT");
@@ -38,7 +48,7 @@ public class LearningEventUtil {
     }
 
     public static void reportStoryBookLearningEvent(StoryBookGson storyBookGson, LearningEventType learningEventType, Context context, String analyticsApplicationId) {
-        Log.i(LearningEventType.class.getName(), "reportStoryBookLearningEvent");
+        Log.i(LearningEventUtil.class.getName(), "reportStoryBookLearningEvent");
 
         Intent broadcastIntent = new Intent();
         broadcastIntent.setAction("ai.elimu.intent.action.STORYBOOK_LEARNING_EVENT");
@@ -47,5 +57,81 @@ public class LearningEventUtil {
         broadcastIntent.putExtra("learningEventType", learningEventType.toString());
         broadcastIntent.setPackage(analyticsApplicationId);
         context.sendBroadcast(broadcastIntent);
+    }
+
+    public static List<WordLearningEventGson> getWordLearningEventGsons(Context context, String analyticsApplicationId) {
+        Log.i(LearningEventUtil.class.getName(), "getWordLearningEventGsons");
+
+        List<WordLearningEventGson> wordLearningEventGsons = new ArrayList<>();
+
+        Uri wordLearningEventsUri = Uri.parse("content://" + analyticsApplicationId + ".provider.word_learning_event_provider/events");
+        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsUri: " + wordLearningEventsUri);
+        Cursor wordLearningEventsCursor = context.getContentResolver().query(wordLearningEventsUri, null, null, null, null);
+        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor: " + wordLearningEventsCursor);
+        if (wordLearningEventsCursor == null) {
+            Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor == null");
+            Toast.makeText(context, "wordLearningEventsCursor == null", Toast.LENGTH_LONG).show();
+        } else {
+            Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount(): " + wordLearningEventsCursor.getCount());
+            if (wordLearningEventsCursor.getCount() == 0) {
+                Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount() == 0");
+            } else {
+                boolean isLast = false;
+                while (!isLast) {
+                    wordLearningEventsCursor.moveToNext();
+
+                    // Convert from Room to Gson
+                    WordLearningEventGson wordLearningEventGson = CursorToWordLearningEventGsonConverter.getWordLearningEventGson(wordLearningEventsCursor);
+
+                    wordLearningEventGsons.add(wordLearningEventGson);
+
+                    isLast = wordLearningEventsCursor.isLast();
+                }
+
+                wordLearningEventsCursor.close();
+                Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.isClosed(): " + wordLearningEventsCursor.isClosed());
+            }
+        }
+        Log.i(LearningEventUtil.class.getName(), "wordLearningEventGsons.size(): " + wordLearningEventGsons.size());
+
+        return wordLearningEventGsons;
+    }
+
+    public static Set<Long> getIdsOfWordsInWordLearningEvents(Context context, String analyticsApplicationId) {
+        Log.i(LearningEventUtil.class.getName(), "getIdsOfWordsInWordLearningEvents");
+
+        Set<Long> wordIdsSet = new HashSet<>();
+
+        Uri wordLearningEventsUri = Uri.parse("content://" + analyticsApplicationId + ".provider.word_learning_event_provider/events");
+        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsUri: " + wordLearningEventsUri);
+        Cursor wordLearningEventsCursor = context.getContentResolver().query(wordLearningEventsUri, null, null, null, null);
+        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor: " + wordLearningEventsCursor);
+        if (wordLearningEventsCursor == null) {
+            Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor == null");
+            Toast.makeText(context, "wordLearningEventsCursor == null", Toast.LENGTH_LONG).show();
+        } else {
+            Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount(): " + wordLearningEventsCursor.getCount());
+            if (wordLearningEventsCursor.getCount() == 0) {
+                Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount() == 0");
+            } else {
+                boolean isLast = false;
+                while (!isLast) {
+                    wordLearningEventsCursor.moveToNext();
+
+                    // Convert from Room to Gson
+                    WordLearningEventGson wordLearningEventGson = CursorToWordLearningEventGsonConverter.getWordLearningEventGson(wordLearningEventsCursor);
+
+                    wordIdsSet.add(wordLearningEventGson.getWordId());
+
+                    isLast = wordLearningEventsCursor.isLast();
+                }
+
+                wordLearningEventsCursor.close();
+                Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.isClosed(): " + wordLearningEventsCursor.isClosed());
+            }
+        }
+        Log.i(LearningEventUtil.class.getName(), "wordIdsSet.size(): " + wordIdsSet.size());
+
+        return wordIdsSet;
     }
 }
