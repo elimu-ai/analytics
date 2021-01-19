78019611bb6f838956e16136ff56a87613dbf2a7
Nya <info@elimu.ai>
2021-01-19 21:13:13 +0100

    refactor: LearningEventUtil

--- utils/src/main/java/ai/elimu/analytics/utils/LearningEventUtil.java
+++ utils/src/main/java/ai/elimu/analytics/utils/LearningEventUtil.java
@@ -2,19 +2,9 @@ package ai.elimu.analytics.utils;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import ai.elimu.analytics.utils.converter.CursorToWordLearningEventGsonConverter;
 import ai.elimu.model.enums.analytics.LearningEventType;
-import ai.elimu.model.v2.gson.analytics.WordLearningEventGson;
 import ai.elimu.model.v2.gson.content.LetterGson;
 import ai.elimu.model.v2.gson.content.StoryBookGson;
 import ai.elimu.model.v2.gson.content.WordGson;
@@ -79,82 +69,5 @@ public class LearningEventUtil {
         broadcastIntent.putExtra("learningEventType", learningEventType.toString());
         broadcastIntent.setPackage(analyticsApplicationId);
         context.sendBroadcast(broadcastIntent);
-    }
-
-
-    public static List<WordLearningEventGson> getWordLearningEventGsons(Context context, String analyticsApplicationId) {
-        Log.i(LearningEventUtil.class.getName(), "getWordLearningEventGsons");
-
-        List<WordLearningEventGson> wordLearningEventGsons = new ArrayList<>();
-
-        Uri wordLearningEventsUri = Uri.parse("content://" + analyticsApplicationId + ".provider.word_learning_event_provider/events");
-        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsUri: " + wordLearningEventsUri);
-        Cursor wordLearningEventsCursor = context.getContentResolver().query(wordLearningEventsUri, null, null, null, null);
-        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor: " + wordLearningEventsCursor);
-        if (wordLearningEventsCursor == null) {
-            Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor == null");
-            Toast.makeText(context, "wordLearningEventsCursor == null", Toast.LENGTH_LONG).show();
-        } else {
-            Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount(): " + wordLearningEventsCursor.getCount());
-            if (wordLearningEventsCursor.getCount() == 0) {
-                Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount() == 0");
-            } else {
-                boolean isLast = false;
-                while (!isLast) {
-                    wordLearningEventsCursor.moveToNext();
-
-                    // Convert from Room to Gson
-                    WordLearningEventGson wordLearningEventGson = CursorToWordLearningEventGsonConverter.getWordLearningEventGson(wordLearningEventsCursor);
-
-                    wordLearningEventGsons.add(wordLearningEventGson);
-
-                    isLast = wordLearningEventsCursor.isLast();
-                }
-
-                wordLearningEventsCursor.close();
-                Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.isClosed(): " + wordLearningEventsCursor.isClosed());
-            }
-        }
-        Log.i(LearningEventUtil.class.getName(), "wordLearningEventGsons.size(): " + wordLearningEventGsons.size());
-
-        return wordLearningEventGsons;
-    }
-
-    public static Set<Long> getIdsOfWordsInWordLearningEvents(Context context, String analyticsApplicationId) {
-        Log.i(LearningEventUtil.class.getName(), "getIdsOfWordsInWordLearningEvents");
-
-        Set<Long> wordIdsSet = new HashSet<>();
-
-        Uri wordLearningEventsUri = Uri.parse("content://" + analyticsApplicationId + ".provider.word_learning_event_provider/events");
-        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsUri: " + wordLearningEventsUri);
-        Cursor wordLearningEventsCursor = context.getContentResolver().query(wordLearningEventsUri, null, null, null, null);
-        Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor: " + wordLearningEventsCursor);
-        if (wordLearningEventsCursor == null) {
-            Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor == null");
-            Toast.makeText(context, "wordLearningEventsCursor == null", Toast.LENGTH_LONG).show();
-        } else {
-            Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount(): " + wordLearningEventsCursor.getCount());
-            if (wordLearningEventsCursor.getCount() == 0) {
-                Log.e(LearningEventUtil.class.getName(), "wordLearningEventsCursor.getCount() == 0");
-            } else {
-                boolean isLast = false;
-                while (!isLast) {
-                    wordLearningEventsCursor.moveToNext();
-
-                    // Convert from Room to Gson
-                    WordLearningEventGson wordLearningEventGson = CursorToWordLearningEventGsonConverter.getWordLearningEventGson(wordLearningEventsCursor);
-
-                    wordIdsSet.add(wordLearningEventGson.getWordId());
-
-                    isLast = wordLearningEventsCursor.isLast();
-                }
-
-                wordLearningEventsCursor.close();
-                Log.i(LearningEventUtil.class.getName(), "wordLearningEventsCursor.isClosed(): " + wordLearningEventsCursor.isClosed());
-            }
-        }
-        Log.i(LearningEventUtil.class.getName(), "wordIdsSet.size(): " + wordIdsSet.size());
-
-        return wordIdsSet;
     }
 }
