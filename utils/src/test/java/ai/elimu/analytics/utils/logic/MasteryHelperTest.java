9e7f5c230ed79e658c37e012f3b2a2f4ef331c11
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-31 16:52:19 +0700

    Fix wrong implementation of isLetterMastered

--- utils/src/test/java/ai/elimu/analytics/utils/logic/MasteryHelperTest.java
+++ utils/src/test/java/ai/elimu/analytics/utils/logic/MasteryHelperTest.java
@@ -44,6 +44,43 @@ public class MasteryHelperTest {
     }
 
     @Test
+    public void testIsLetterMastered_NotRecent_False() {
+        List<LetterAssessmentEventGson> letterAssessmentEventGsons = new ArrayList<>();
+        assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
+
+        LetterAssessmentEventGson letterAssessmentEventGsonNow = new LetterAssessmentEventGson();
+        Calendar calendarNow = Calendar.getInstance();
+        letterAssessmentEventGsonNow.setTime(calendarNow);
+        letterAssessmentEventGsonNow.setMasteryScore(0.00f);
+        letterAssessmentEventGsons.add(letterAssessmentEventGsonNow);
+        assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
+
+        LetterAssessmentEventGson letterAssessmentEventGson10MinutesAgo = new LetterAssessmentEventGson();
+        Calendar calendar10MinutesAgo = Calendar.getInstance();
+        calendar10MinutesAgo.add(Calendar.MINUTE, -10);
+        letterAssessmentEventGson10MinutesAgo.setTime(calendar10MinutesAgo);
+        letterAssessmentEventGson10MinutesAgo.setMasteryScore(1.00f);
+        letterAssessmentEventGsons.add(letterAssessmentEventGson10MinutesAgo);
+        assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
+
+        LetterAssessmentEventGson letterAssessmentEventGson20MinutesAgo = new LetterAssessmentEventGson();
+        Calendar calendar20MinutesAgo = Calendar.getInstance();
+        calendar20MinutesAgo.add(Calendar.MINUTE, -20);
+        letterAssessmentEventGson20MinutesAgo.setTime(calendar20MinutesAgo);
+        letterAssessmentEventGson20MinutesAgo.setMasteryScore(1.00f);
+        letterAssessmentEventGsons.add(letterAssessmentEventGson20MinutesAgo);
+        assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
+
+        LetterAssessmentEventGson letterAssessmentEventGson30MinutesAgo = new LetterAssessmentEventGson();
+        Calendar calendar30MinutesAgo = Calendar.getInstance();
+        calendar20MinutesAgo.add(Calendar.MINUTE, -30);
+        letterAssessmentEventGson30MinutesAgo.setTime(calendar30MinutesAgo);
+        letterAssessmentEventGson30MinutesAgo.setMasteryScore(1.00f);
+        letterAssessmentEventGsons.add(letterAssessmentEventGson30MinutesAgo);
+        assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
+    }
+
+    @Test
     public void testIsLetterMastered_True() {
         List<LetterAssessmentEventGson> letterAssessmentEventGsons = new ArrayList<>();
         assertThat(MasteryHelper.isLetterMastered(letterAssessmentEventGsons), is(false));
@@ -108,6 +145,7 @@ public class MasteryHelperTest {
         wordAssessmentEventGsons.add(wordAssessmentEventGson20MinutesAgo);
         assertThat(MasteryHelper.isWordMastered(wordAssessmentEventGsons), is(false));
     }
+
 
     @Test
     public void testIsWordMastered_True() {
