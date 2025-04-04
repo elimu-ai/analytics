996f4e6ed67766cc30b88eb3af473a1545c41782
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-04 10:36:16 +0700

    Convert Upload service classes to Kotlin

--- app/src/main/java/ai/elimu/analytics/rest/LetterLearningEventService.kt
+++ app/src/main/java/ai/elimu/analytics/rest/LetterLearningEventService.kt
@@ -1,15 +1,14 @@
-package ai.elimu.analytics.rest;
+package ai.elimu.analytics.rest
 
-import okhttp3.MultipartBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
-import retrofit2.http.Part;
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
-public interface LetterLearningEventService {
-
+interface LetterLearningEventService {
     @Multipart
     @POST("analytics/letter-learning-events/csv")
-    Call<ResponseBody> uploadCsvFile(@Part MultipartBody.Part part);
+    fun uploadCsvFile(@Part part: MultipartBody.Part?): Call<ResponseBody?>
 }
