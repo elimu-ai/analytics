3b2c3767903555d524114d72eb3435d7b70ba423
tuancoltech <tuancoltech@gmail.com>
2025-04-06 15:36:56 +0700

    Convert EventListActivity to Kotlin

--- app/src/main/java/ai/elimu/analytics/EventListActivity.kt
+++ app/src/main/java/ai/elimu/analytics/EventListActivity.kt
@@ -1,50 +1,42 @@
-package ai.elimu.analytics;
+package ai.elimu.analytics
 
-import android.os.Bundle;
+import ai.elimu.analytics.db.RoomDb
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import timber.log.Timber
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class EventListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.i("onCreate")
+        super.onCreate(savedInstanceState)
 
-import java.util.List;
+        setContentView(R.layout.activity_event_list)
 
-import ai.elimu.analytics.dao.StoryBookLearningEventDao;
-import ai.elimu.analytics.db.RoomDb;
-import ai.elimu.analytics.entity.StoryBookLearningEvent;
-import timber.log.Timber;
-
-public class EventListActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        Timber.i("onCreate");
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_event_list);
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final EventListAdapter eventListAdapter = new EventListAdapter(this);
-        recyclerView.setAdapter(eventListAdapter);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
-        recyclerView.addItemDecoration(dividerItemDecoration);
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val eventListAdapter = EventListAdapter(this)
+        recyclerView.adapter = eventListAdapter
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+        val dividerItemDecoration =
+            DividerItemDecoration(recyclerView.context, linearLayoutManager.orientation)
+        recyclerView.addItemDecoration(dividerItemDecoration)
 
         // Fetch all learning events from database, and update adapter
-        RoomDb roomDb = RoomDb.getDatabase(getApplicationContext());
-        StoryBookLearningEventDao storyBookLearningEventDao = roomDb.storyBookLearningEventDao();
-        RoomDb.databaseWriteExecutor.execute(() -> {
-            List<StoryBookLearningEvent> storyBookLearningEvents = storyBookLearningEventDao.loadAll();
-            Timber.d("storyBookLearningEvents.size(): " + storyBookLearningEvents.size());
-            eventListAdapter.setStoryBookLearningEvents(storyBookLearningEvents);
-        });
+        val roomDb = RoomDb.getDatabase(applicationContext)
+        val storyBookLearningEventDao = roomDb.storyBookLearningEventDao()
+        RoomDb.databaseWriteExecutor.execute {
+            val storyBookLearningEvents =
+                storyBookLearningEventDao.loadAll()
+            Timber.d("storyBookLearningEvents.size(): " + storyBookLearningEvents.size)
+            eventListAdapter.setStoryBookLearningEvents(storyBookLearningEvents)
+        }
     }
 
-    @Override
-    protected void onStart() {
-        Timber.i( "onStart");
-        super.onStart();
+    override fun onStart() {
+        Timber.i("onStart")
+        super.onStart()
     }
 }
