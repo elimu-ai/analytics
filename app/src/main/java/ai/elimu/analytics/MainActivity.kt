59776cfcd57d70b00ade374e34edb73c2472904b
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-17 10:02:53 +0700

    Convert MainActivity to Kotlin

--- app/src/main/java/ai/elimu/analytics/MainActivity.kt
+++ app/src/main/java/ai/elimu/analytics/MainActivity.kt
@@ -1,66 +1,61 @@
-package ai.elimu.analytics;
+package ai.elimu.analytics
 
-import android.content.Intent;
-import android.os.Bundle;
+import ai.elimu.analytics.db.RoomDb
+import ai.elimu.analytics.language.SelectLanguageActivity
+import ai.elimu.analytics.task.TaskInitializer
+import ai.elimu.analytics.util.SharedPreferencesHelper.getLanguage
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.i("onCreate")
+        super.onCreate(savedInstanceState)
 
-import java.util.List;
-
-import ai.elimu.analytics.dao.StoryBookLearningEventDao;
-import ai.elimu.analytics.dao.WordLearningEventDao;
-import ai.elimu.analytics.db.RoomDb;
-import ai.elimu.analytics.entity.StoryBookLearningEvent;
-import ai.elimu.analytics.entity.WordLearningEvent;
-import ai.elimu.analytics.language.SelectLanguageActivity;
-import ai.elimu.analytics.task.TaskInitializer;
-import ai.elimu.analytics.util.SharedPreferencesHelper;
-import ai.elimu.model.v2.enums.Language;
-import timber.log.Timber;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Timber.i("onCreate");
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
     }
 
-    @Override
-    protected void onStart() {
-        Timber.i("onStart");
-        super.onStart();
+    override fun onStart() {
+        Timber.i("onStart")
+        super.onStart()
 
-        Language language = SharedPreferencesHelper.getLanguage(getApplicationContext());
-        Timber.i("language: " + language);
+        val language = getLanguage(
+            applicationContext
+        )
+        Timber.i("language: $language")
         if (language == null) {
             // Redirect to language selection
 
-            Intent selectLanguageIntent = new Intent(getApplicationContext(), SelectLanguageActivity.class);
-            startActivity(selectLanguageIntent);
-            finish();
+            val selectLanguageIntent = Intent(
+                applicationContext,
+                SelectLanguageActivity::class.java
+            )
+            startActivity(selectLanguageIntent)
+            finish()
         } else {
             // Redirect to event list
 
-            TaskInitializer.initializePeriodicWork(getApplicationContext());
+            TaskInitializer.initializePeriodicWork(applicationContext)
 
             // If the database is not empty, redirect the user to the EventListActivity
-            RoomDb roomDb = RoomDb.getDatabase(getApplicationContext());
-            StoryBookLearningEventDao storyBookLearningEventDao = roomDb.storyBookLearningEventDao();
-            WordLearningEventDao wordLearningEventDao = roomDb.wordLearningEventDao();
-            RoomDb.databaseWriteExecutor.execute(() -> {
-                List<WordLearningEvent> wordLearningEvents = wordLearningEventDao.loadAllOrderedByTimeDesc();
-                Timber.i("wordLearningEvents.size(): " + wordLearningEvents.size());
+            val roomDb = RoomDb.getDatabase(applicationContext)
+            val storyBookLearningEventDao = roomDb.storyBookLearningEventDao()
+            val wordLearningEventDao = roomDb.wordLearningEventDao()
+            RoomDb.databaseWriteExecutor.execute {
+                val wordLearningEvents =
+                    wordLearningEventDao.loadAllOrderedByTimeDesc()
+                Timber.i("wordLearningEvents.size(): " + wordLearningEvents.size)
 
-                List<StoryBookLearningEvent> storyBookLearningEvents = storyBookLearningEventDao.loadAll();
-                Timber.i("storyBookLearningEvents.size(): " + storyBookLearningEvents.size());
-                if (!storyBookLearningEvents.isEmpty()) {
-                    startActivity(new Intent(getApplicationContext(), EventListActivity.class));
-                    finish();
+                val storyBookLearningEvents =
+                    storyBookLearningEventDao.loadAll()
+                Timber.i("storyBookLearningEvents.size(): " + storyBookLearningEvents.size)
+                if (storyBookLearningEvents.isNotEmpty()) {
+                    startActivity(Intent(applicationContext, EventListActivity::class.java))
+                    finish()
                 }
-            });
+            }
         }
     }
 }
