c0d932a56c46aaf7ffe86ac5e8bd0d53bb9a09c1
tuancoltech <tuancoltech@gmail.com>
2025-04-17 22:56:54 +0700

    Convert StoryBookLearningEventReceiver to Kotlin

--- app/src/main/java/ai/elimu/analytics/receiver/StoryBookLearningEventReceiver.kt
+++ app/src/main/java/ai/elimu/analytics/receiver/StoryBookLearningEventReceiver.kt
@@ -1,57 +1,55 @@
-package ai.elimu.analytics.receiver;
+package ai.elimu.analytics.receiver
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.provider.Settings;
+import ai.elimu.analytics.db.RoomDb
+import ai.elimu.analytics.entity.StoryBookLearningEvent
+import ai.elimu.model.v2.enums.analytics.LearningEventType
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import timber.log.Timber
+import java.util.Calendar
 
-import java.util.Calendar;
+class StoryBookLearningEventReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Timber.i("onReceive")
 
-import ai.elimu.analytics.dao.StoryBookLearningEventDao;
-import ai.elimu.analytics.db.RoomDb;
-import ai.elimu.analytics.entity.StoryBookLearningEvent;
-import ai.elimu.model.v2.enums.analytics.LearningEventType;
-import timber.log.Timber;
+        val androidId =
+            Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        Timber.i("androidId: \"$androidId\"")
 
-public class StoryBookLearningEventReceiver extends BroadcastReceiver {
+        val packageName = intent.getStringExtra("packageName")
+        Timber.i("packageName: \"$packageName\"")
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Timber.i("onReceive");
+        val timestamp = Calendar.getInstance()
+        Timber.i("timestamp.getTime(): " + timestamp.time)
 
-        String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-        Timber.i("androidId: \"" + androidId + "\"");
+        val storyBookId = intent.getLongExtra("storyBookId", 0)
+        Timber.i("storyBookId: $storyBookId")
 
-        String packageName = intent.getStringExtra("packageName");
-        Timber.i("packageName: \"" + packageName + "\"");
+        val storyBookTitle = intent.getStringExtra("storyBookTitle")
+        Timber.i("storyBookTitle: \"$storyBookTitle\"")
 
-        Calendar timestamp = Calendar.getInstance();
-        Timber.i("timestamp.getTime(): " + timestamp.getTime());
+        val learningEventTypeAsString = intent.getStringExtra("learningEventType")
+        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
+        val learningEventType = LearningEventType.valueOf(
+            learningEventTypeAsString!!
+        )
+        Timber.i("learningEventType: $learningEventType")
 
-        Long storyBookId = intent.getLongExtra("storyBookId", 0);
-        Timber.i("storyBookId: " + storyBookId);
-
-        String storyBookTitle = intent.getStringExtra("storyBookTitle");
-        Timber.i("storyBookTitle: \"" + storyBookTitle + "\"");
-
-        String learningEventTypeAsString = intent.getStringExtra("learningEventType");
-        Timber.i("learningEventTypeAsString: \"" + learningEventTypeAsString + "\"");
-        LearningEventType learningEventType = LearningEventType.valueOf(learningEventTypeAsString);
-        Timber.i("learningEventType: " + learningEventType);
-
-        StoryBookLearningEvent storyBookLearningEvent = new StoryBookLearningEvent();
-        storyBookLearningEvent.androidId = androidId;
-        storyBookLearningEvent.packageName = packageName;
-        storyBookLearningEvent.time = timestamp;
-        storyBookLearningEvent.setStoryBookId(storyBookId);
-//        storyBookLearningEvent.setStoryBookTitle(storyBookTitle);
-        storyBookLearningEvent.setLearningEventType(learningEventType);
+        val storyBookLearningEvent = StoryBookLearningEvent()
+        storyBookLearningEvent.androidId = androidId
+        storyBookLearningEvent.packageName = packageName!!
+        storyBookLearningEvent.time = timestamp
+        storyBookLearningEvent.storyBookId = storyBookId
+        //        storyBookLearningEvent.setStoryBookTitle(storyBookTitle);
+        storyBookLearningEvent.learningEventType = learningEventType
 
         // Store in database
-        RoomDb roomDb = RoomDb.getDatabase(context);
-        StoryBookLearningEventDao storyBookLearningEventDao = roomDb.storyBookLearningEventDao();
-        RoomDb.databaseWriteExecutor.execute(() -> {
-            storyBookLearningEventDao.insert(storyBookLearningEvent);
-        });
+        val roomDb = RoomDb.getDatabase(context)
+        val storyBookLearningEventDao = roomDb.storyBookLearningEventDao()
+        RoomDb.databaseWriteExecutor.execute {
+            storyBookLearningEventDao.insert(storyBookLearningEvent)
+        }
     }
 }
