393a4d8ea0fbca9d33add7b5a324938ceb48ff40
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-18 14:08:55 +0700

    Convert EventListAdapter to Kotlin

--- app/src/main/java/ai/elimu/analytics/EventListAdapter.kt
+++ app/src/main/java/ai/elimu/analytics/EventListAdapter.kt
@@ -1,72 +1,54 @@
-package ai.elimu.analytics;
+package ai.elimu.analytics
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import ai.elimu.analytics.EventListAdapter.EventViewHolder
+import ai.elimu.analytics.entity.StoryBookLearningEvent
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-import ai.elimu.analytics.entity.StoryBookLearningEvent;
-
-public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder> {
-
-    class EventViewHolder extends RecyclerView.ViewHolder {
-
-        private final TextView textViewFirstLine;
-        private final TextView textViewSecondLine;
-
-        private EventViewHolder(View itemView) {
-            super(itemView);
-            textViewFirstLine = itemView.findViewById(R.id.textViewFirstLine);
-            textViewSecondLine = itemView.findViewById(R.id.textViewSecondLine);
-        }
+class EventListAdapter internal constructor(context: Context?) :
+    RecyclerView.Adapter<EventViewHolder>() {
+    inner class EventViewHolder internal constructor(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        val textViewFirstLine: TextView = itemView.findViewById(R.id.textViewFirstLine)
+        val textViewSecondLine: TextView = itemView.findViewById(R.id.textViewSecondLine)
     }
 
-    private final LayoutInflater layoutInflater;
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
-    private List<StoryBookLearningEvent> storyBookLearningEvents;
+    private var storyBookLearningEvents: List<StoryBookLearningEvent>? = null
 
-    EventListAdapter(Context context) {
-        layoutInflater = LayoutInflater.from(context);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.activity_event_list_item, parent, false)
+        return EventViewHolder(itemView)
     }
 
-    @NonNull
-    @Override
-    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = layoutInflater.inflate(R.layout.activity_event_list_item, parent, false);
-        return new EventViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull EventViewHolder viewHolder, int position) {
+    override fun onBindViewHolder(viewHolder: EventViewHolder, position: Int) {
         if (storyBookLearningEvents != null) {
-            StoryBookLearningEvent storyBookLearningEvent = storyBookLearningEvents.get(position);
-            viewHolder.textViewFirstLine.setText("StoryBookLearningEvent");
-            viewHolder.textViewSecondLine.setText("id: " + storyBookLearningEvent.getId()
-                    + ", time: " + storyBookLearningEvent.time.getTime()
+            val storyBookLearningEvent = storyBookLearningEvents!![position]
+            viewHolder.textViewFirstLine.text = "StoryBookLearningEvent"
+            viewHolder.textViewSecondLine.text = ("id: " + storyBookLearningEvent.id
+                    + ", time: " + storyBookLearningEvent.time.time
                     + ", androidId: \"" + storyBookLearningEvent.androidId + "\""
                     + ", packageName: \"" + storyBookLearningEvent.packageName + "\""
-                    + ", storyBookId: " + storyBookLearningEvent.getStoryBookId()
-                    + ", learningEventType: \"" + storyBookLearningEvent.getLearningEventType() + "\"");
+                    + ", storyBookId: " + storyBookLearningEvent.storyBookId
+                    + ", learningEventType: \"" + storyBookLearningEvent.learningEventType + "\"")
         }
     }
 
-    @Override
-    public int getItemCount() {
-        if (storyBookLearningEvents == null) {
-            return 0;
+    override fun getItemCount(): Int {
+        return if (storyBookLearningEvents == null) {
+            0
         } else {
-            return storyBookLearningEvents.size();
+            storyBookLearningEvents!!.size
         }
     }
 
-    public void setStoryBookLearningEvents(List<StoryBookLearningEvent> storyBookLearningEvents) {
-        this.storyBookLearningEvents = storyBookLearningEvents;
-        notifyDataSetChanged();
+    fun setStoryBookLearningEvents(storyBookLearningEvents: List<StoryBookLearningEvent>?) {
+        this.storyBookLearningEvents = storyBookLearningEvents
+        notifyDataSetChanged()
     }
 }
