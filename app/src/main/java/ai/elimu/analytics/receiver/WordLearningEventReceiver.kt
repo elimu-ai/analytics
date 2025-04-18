f342aed46a9c24ffc454e14969f8b8328b929e8d
tuancoltech <tuancoltech@gmail.com>
2025-04-18 12:30:39 +0700

    Convert WordLearningEventReceiver to Kotlin

--- app/src/main/java/ai/elimu/analytics/receiver/WordLearningEventReceiver.kt
+++ app/src/main/java/ai/elimu/analytics/receiver/WordLearningEventReceiver.kt
@@ -1,60 +1,58 @@
-package ai.elimu.analytics.receiver;
+package ai.elimu.analytics.receiver
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.provider.Settings;
+import ai.elimu.analytics.db.RoomDb
+import ai.elimu.analytics.entity.WordLearningEvent
+import ai.elimu.model.v2.enums.analytics.LearningEventType
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import timber.log.Timber
+import java.util.Calendar
 
-import java.util.Calendar;
+class WordLearningEventReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Timber.i("onReceive")
 
-import ai.elimu.analytics.dao.WordLearningEventDao;
-import ai.elimu.analytics.db.RoomDb;
-import ai.elimu.analytics.entity.WordLearningEvent;
-import ai.elimu.model.v2.enums.analytics.LearningEventType;
-import timber.log.Timber;
+        val androidId =
+            Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        Timber.i("androidId: \"$androidId\"")
 
-public class WordLearningEventReceiver extends BroadcastReceiver {
+        val packageName = intent.getStringExtra("packageName")
+        Timber.i("packageName: \"$packageName\"")
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Timber.i("onReceive");
+        val timestamp = Calendar.getInstance()
+        Timber.i("timestamp.getTime(): " + timestamp.time)
 
-        String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-        Timber.i("androidId: \"" + androidId + "\"");
-
-        String packageName = intent.getStringExtra("packageName");
-        Timber.i("packageName: \"" + packageName + "\"");
-
-        Calendar timestamp = Calendar.getInstance();
-        Timber.i("timestamp.getTime(): " + timestamp.getTime());
-
-        Long wordId = null;
+        var wordId: Long? = null
         if (intent.hasExtra("wordId")) {
-            wordId = intent.getLongExtra("wordId", 0);
+            wordId = intent.getLongExtra("wordId", 0)
         }
-        Timber.i("wordId: " + wordId);
+        Timber.i("wordId: $wordId")
 
-        String wordText = intent.getStringExtra("wordText");
-        Timber.i("wordText: \"" + wordText + "\"");
+        val wordText = intent.getStringExtra("wordText")
+        Timber.i("wordText: \"$wordText\"")
 
-        String learningEventTypeAsString = intent.getStringExtra("learningEventType");
-        Timber.i("learningEventTypeAsString: \"" + learningEventTypeAsString + "\"");
-        LearningEventType learningEventType = LearningEventType.valueOf(learningEventTypeAsString);
-        Timber.i("learningEventType: " + learningEventType);
+        val learningEventTypeAsString = intent.getStringExtra("learningEventType")
+        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
+        val learningEventType = LearningEventType.valueOf(
+            learningEventTypeAsString!!
+        )
+        Timber.i("learningEventType: $learningEventType")
 
-        WordLearningEvent wordLearningEvent = new WordLearningEvent();
-        wordLearningEvent.androidId = androidId;
-        wordLearningEvent.packageName = packageName;
-        wordLearningEvent.time = timestamp;
-        wordLearningEvent.setWordId(wordId);
-        wordLearningEvent.setWordText(wordText);
-        wordLearningEvent.setLearningEventType(learningEventType);
+        val wordLearningEvent = WordLearningEvent()
+        wordLearningEvent.androidId = androidId
+        wordLearningEvent.packageName = packageName!!
+        wordLearningEvent.time = timestamp
+        wordLearningEvent.wordId = wordId
+        wordLearningEvent.wordText = wordText
+        wordLearningEvent.learningEventType = learningEventType
 
         // Store in database
-        RoomDb roomDb = RoomDb.getDatabase(context);
-        WordLearningEventDao wordLearningEventDao = roomDb.wordLearningEventDao();
-        RoomDb.databaseWriteExecutor.execute(() -> {
-            wordLearningEventDao.insert(wordLearningEvent);
-        });
+        val roomDb = RoomDb.getDatabase(context)
+        val wordLearningEventDao = roomDb.wordLearningEventDao()
+        RoomDb.databaseWriteExecutor.execute {
+            wordLearningEventDao.insert(wordLearningEvent)
+        }
     }
 }
