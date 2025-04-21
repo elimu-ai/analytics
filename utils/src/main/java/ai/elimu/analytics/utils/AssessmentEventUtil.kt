ce3c19d1cf54f86e66def229b100bb500c0396f8
tuancoltech <tuancoltech@gmail.com>
2025-04-21 12:42:46 +0700

    Convert AssessmentEventUtil to Kotlin

--- utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.kt
+++ utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.kt
@@ -1,42 +1,55 @@
-package ai.elimu.analytics.utils;
+package ai.elimu.analytics.utils
 
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-
-import ai.elimu.model.v2.gson.content.LetterGson;
-import ai.elimu.model.v2.gson.content.WordGson;
+import ai.elimu.model.v2.gson.content.LetterGson
+import ai.elimu.model.v2.gson.content.WordGson
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 
 /**
  * A utility class that makes it easier for other apps to report assessments events.
  */
-public class AssessmentEventUtil {
+object AssessmentEventUtil {
 
-    public static void reportLetterAssessmentEvent(LetterGson letterGson, Float masteryScore, Long timeSpentMs, Context context, String analyticsApplicationId) {
-        Log.i(AssessmentEventUtil.class.getName(), "reportLetterAssessmentEvent");
+    private const val TAG = "AssessmentEventUtil"
 
-        Intent broadcastIntent = new Intent();
-        broadcastIntent.setAction("ai.elimu.intent.action.LETTER_ASSESSMENT_EVENT");
-        broadcastIntent.putExtra("packageName", context.getPackageName());
-        broadcastIntent.putExtra("letterId", letterGson.getId());
-        broadcastIntent.putExtra("letterText", letterGson.getText());
-        broadcastIntent.putExtra("masteryScore", masteryScore);
-        broadcastIntent.putExtra("timeSpentMs", timeSpentMs);
-        broadcastIntent.setPackage(analyticsApplicationId);
-        context.sendBroadcast(broadcastIntent);
+    fun reportLetterAssessmentEvent(
+        letterGson: LetterGson,
+        masteryScore: Float?,
+        timeSpentMs: Long?,
+        context: Context,
+        analyticsApplicationId: String?
+    ) {
+        Log.i(TAG, "reportLetterAssessmentEvent")
+
+        val broadcastIntent = Intent()
+        broadcastIntent.setAction("ai.elimu.intent.action.LETTER_ASSESSMENT_EVENT")
+        broadcastIntent.putExtra("packageName", context.packageName)
+        broadcastIntent.putExtra("letterId", letterGson.id)
+        broadcastIntent.putExtra("letterText", letterGson.text)
+        broadcastIntent.putExtra("masteryScore", masteryScore)
+        broadcastIntent.putExtra("timeSpentMs", timeSpentMs)
+        broadcastIntent.setPackage(analyticsApplicationId)
+        context.sendBroadcast(broadcastIntent)
     }
 
-    public static void reportWordAssessmentEvent(WordGson wordGson, Float masteryScore, Long timeSpentMs, Context context, String analyticsApplicationId) {
-        Log.i(AssessmentEventUtil.class.getName(),"reportWordAssessmentEvent");
+    fun reportWordAssessmentEvent(
+        wordGson: WordGson,
+        masteryScore: Float?,
+        timeSpentMs: Long?,
+        context: Context,
+        analyticsApplicationId: String?
+    ) {
+        Log.i(TAG, "reportWordAssessmentEvent")
 
-        Intent broadcastIntent = new Intent();
-        broadcastIntent.setAction("ai.elimu.intent.action.WORD_ASSESSMENT_EVENT");
-        broadcastIntent.putExtra("packageName", context.getPackageName());
-        broadcastIntent.putExtra("wordId", wordGson.getId());
-        broadcastIntent.putExtra("wordText", wordGson.getText());
-        broadcastIntent.putExtra("masteryScore", masteryScore);
-        broadcastIntent.putExtra("timeSpentMs", timeSpentMs);
-        broadcastIntent.setPackage(analyticsApplicationId);
-        context.sendBroadcast(broadcastIntent);
+        val broadcastIntent = Intent()
+        broadcastIntent.setAction("ai.elimu.intent.action.WORD_ASSESSMENT_EVENT")
+        broadcastIntent.putExtra("packageName", context.packageName)
+        broadcastIntent.putExtra("wordId", wordGson.id)
+        broadcastIntent.putExtra("wordText", wordGson.text)
+        broadcastIntent.putExtra("masteryScore", masteryScore)
+        broadcastIntent.putExtra("timeSpentMs", timeSpentMs)
+        broadcastIntent.setPackage(analyticsApplicationId)
+        context.sendBroadcast(broadcastIntent)
     }
 }
