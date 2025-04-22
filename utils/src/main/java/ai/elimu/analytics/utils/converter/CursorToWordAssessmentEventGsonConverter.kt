d791899a6614466ab3c1ddb0e81def2ad5c9de02
tuancoltech <tuancoltech@gmail.com>
2025-04-22 23:04:40 +0700

    Extract log tag to a global const value

--- utils/src/main/java/ai/elimu/analytics/utils/converter/CursorToWordAssessmentEventGsonConverter.kt
+++ utils/src/main/java/ai/elimu/analytics/utils/converter/CursorToWordAssessmentEventGsonConverter.kt
@@ -6,76 +6,79 @@ import android.util.Log
 import java.util.Calendar
 
 object CursorToWordAssessmentEventGsonConverter {
+    
+    private const val TAG = "CursorToWordAssessmentEventGsonConverter"
+    
     fun getWordAssessmentEventGson(cursor: Cursor): WordAssessmentEventGson {
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "getWordAssessmentEventGson"
         )
 
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "Arrays.toString(cursor.getColumnNames()): " + cursor.columnNames.contentToString()
         )
 
         val columnId = cursor.getColumnIndex("id")
         val id = cursor.getLong(columnId)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "id: $id"
         )
 
         val columnAndroidId = cursor.getColumnIndex("androidId")
         val androidId = cursor.getString(columnAndroidId)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "androidId: \"$androidId\""
         )
 
         val columnPackageName = cursor.getColumnIndex("packageName")
         val packageName = cursor.getString(columnPackageName)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "packageName: \"$packageName\""
         )
 
         val columnTime = cursor.getColumnIndex("time")
         val timeAsLong = cursor.getLong(columnTime)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "timeAsLong: $timeAsLong"
         )
         val timestamp = Calendar.getInstance()
         timestamp.timeInMillis = timeAsLong
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "time.getTime(): " + timestamp.time
         )
 
         val columnWordId = cursor.getColumnIndex("wordId")
         val wordId = cursor.getLong(columnWordId)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "wordId: $wordId"
         )
 
         val columnWordText = cursor.getColumnIndex("wordText")
         val wordText = cursor.getString(columnWordText)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "wordText: \"$wordText\""
         )
 
         val columnMasteryScore = cursor.getColumnIndex("masteryScore")
         val masteryScore = cursor.getFloat(columnMasteryScore)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "masteryScore: $masteryScore"
         )
 
         val columnTimeSpentMs = cursor.getColumnIndex("timeSpentMs")
         val timeSpentMs = cursor.getLong(columnTimeSpentMs)
         Log.i(
-            CursorToWordAssessmentEventGsonConverter::class.java.name,
+            TAG,
             "timeSpentMs: $masteryScore"
         )
 
