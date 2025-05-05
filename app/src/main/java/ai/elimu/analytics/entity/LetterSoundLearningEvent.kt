1b03f7579525783f8c724589b89305bd756c254d
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-05 08:44:24 +0000

    Convert LetterSoundLearningEvent to Kotlin

--- app/src/main/java/ai/elimu/analytics/entity/LetterSoundLearningEvent.kt
+++ app/src/main/java/ai/elimu/analytics/entity/LetterSoundLearningEvent.kt
@@ -1,42 +1,15 @@
-package ai.elimu.analytics.entity;
+package ai.elimu.analytics.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 @Entity
-public class LetterSoundLearningEvent extends LearningEvent {
+class LetterSoundLearningEvent : LearningEvent() {
+    @JvmField
+    var letterSoundId: Long? = null
 
-    private Long letterSoundId;
+    @JvmField
+    var letterSoundLetterTexts: Array<String> = arrayOf()
 
-    @NonNull
-    private String[] letterSoundLetterTexts;
-
-    @NonNull
-    private String[] letterSoundSoundValuesIpa;
-
-    public Long getLetterSoundId() {
-        return letterSoundId;
-    }
-
-    public void setLetterSoundId(Long letterSoundId) {
-        this.letterSoundId = letterSoundId;
-    }
-
-    @NonNull
-    public String[] getLetterSoundLetterTexts() {
-        return letterSoundLetterTexts;
-    }
-
-    public void setLetterSoundLetterTexts(@NonNull String[] letterSoundLetterTexts) {
-        this.letterSoundLetterTexts = letterSoundLetterTexts;
-    }
-
-    @NonNull
-    public String[] getLetterSoundSoundValuesIpa() {
-        return letterSoundSoundValuesIpa;
-    }
-
-    public void setLetterSoundSoundValuesIpa(@NonNull String[] letterSoundSoundValuesIpa) {
-        this.letterSoundSoundValuesIpa = letterSoundSoundValuesIpa;
-    }
+    @JvmField
+    var letterSoundSoundValuesIpa: Array<String> = arrayOf()
 }
