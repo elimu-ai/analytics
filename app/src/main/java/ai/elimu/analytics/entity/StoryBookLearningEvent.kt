742b9882925649fa81315e526c878894480037c4
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-05 08:47:56 +0000

    Convert StoryBookLearningEvent to Kotlin

--- app/src/main/java/ai/elimu/analytics/entity/StoryBookLearningEvent.kt
+++ app/src/main/java/ai/elimu/analytics/entity/StoryBookLearningEvent.kt
@@ -1,32 +1,12 @@
-package ai.elimu.analytics.entity;
+package ai.elimu.analytics.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-
-import ai.elimu.model.v2.enums.analytics.LearningEventType;
+import ai.elimu.model.v2.enums.analytics.LearningEventType
+import androidx.room.Entity
 
 @Entity
-public class StoryBookLearningEvent extends LearningEvent {
+class StoryBookLearningEvent : LearningEvent() {
+    @JvmField
+    var storyBookId: Long = 0L
 
-    @NonNull
-    private Long storyBookId;
-
-    @NonNull
-    private LearningEventType learningEventType;
-
-    public Long getStoryBookId() {
-        return storyBookId;
-    }
-
-    public void setStoryBookId(Long storyBookId) {
-        this.storyBookId = storyBookId;
-    }
-
-    public LearningEventType getLearningEventType() {
-        return learningEventType;
-    }
-
-    public void setLearningEventType(LearningEventType learningEventType) {
-        this.learningEventType = learningEventType;
-    }
+    lateinit var learningEventType: LearningEventType
 }
