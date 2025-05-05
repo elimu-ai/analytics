3d357671d233c923909f92a2cefb6f279ecf71ec
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-05 08:29:57 +0000

    Convert AssessmentEvent to Kotlin

--- app/src/main/java/ai/elimu/analytics/entity/AssessmentEvent.kt
+++ app/src/main/java/ai/elimu/analytics/entity/AssessmentEvent.kt
@@ -1,43 +1,15 @@
-package ai.elimu.analytics.entity;
+package ai.elimu.analytics.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-
-import java.util.Calendar;
+import androidx.room.Entity
+import java.util.Calendar
 
 @Entity
-public abstract class AssessmentEvent extends BaseEntity {
+abstract class AssessmentEvent : BaseEntity() {
+    @JvmField
+    var androidId: String = ""
 
-    @NonNull
-    private String androidId;
+    @JvmField
+    var packageName: String = ""
 
-    @NonNull
-    private String packageName;
-
-    @NonNull
-    private Calendar time;
-
-    public String getAndroidId() {
-        return androidId;
-    }
-
-    public void setAndroidId(String androidId) {
-        this.androidId = androidId;
-    }
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    public Calendar getTime() {
-        return time;
-    }
-
-    public void setTime(Calendar time) {
-        this.time = time;
-    }
+    lateinit var time: Calendar
 }
