8af41ecc013d1d331e85621b4914e74477b7f4f7
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-05-28 23:55:58 +0700

    feat: add letter-sound assessment events to utils lib
    #128

--- utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.kt
+++ utils/src/main/java/ai/elimu/analytics/utils/AssessmentEventUtil.kt
@@ -1,10 +1,13 @@
 package ai.elimu.analytics.utils
 
 import ai.elimu.model.v2.gson.content.LetterGson
+import ai.elimu.model.v2.gson.content.LetterSoundGson
+import ai.elimu.model.v2.gson.content.SoundGson
 import ai.elimu.model.v2.gson.content.WordGson
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import java.util.stream.Collectors
 
 /**
  * A utility class that makes it easier for other apps to report assessments events.
@@ -12,6 +15,29 @@ import android.util.Log
 object AssessmentEventUtil {
 
     private const val TAG = "AssessmentEventUtil"
+
+    fun reportLetterSoundAssessmentEvent(
+        letterSoundGson: LetterSoundGson,
+        masteryScore: Float,
+        timeSpentMs: Long,
+        additionalData: String,
+        context: Context,
+        analyticsApplicationId: String
+    ) {
+        Log.i(TAG,"reportLetterSoundAssessmentEvent")
+
+        val broadcastIntent = Intent()
+        broadcastIntent.setAction("ai.elimu.intent.action.LETTER_SOUND_ASSESSMENT_EVENT")
+        broadcastIntent.putExtra("packageName", context.packageName)
+        broadcastIntent.putExtra("letterSoundLetters", letterSoundGson.letters.stream().map(LetterGson::getText).collect(Collectors.joining()))
+        broadcastIntent.putExtra("letterSoundSounds", letterSoundGson.sounds.stream().map(SoundGson::getValueIpa).collect(Collectors.joining()))
+        broadcastIntent.putExtra("letterSoundId", letterSoundGson.id)
+        broadcastIntent.putExtra("masteryScore", masteryScore)
+        broadcastIntent.putExtra("timeSpentMs", timeSpentMs)
+        broadcastIntent.putExtra("additionalData", additionalData)
+        broadcastIntent.setPackage(analyticsApplicationId)
+        context.sendBroadcast(broadcastIntent)
+    }
 
     fun reportLetterAssessmentEvent(
         letterGson: LetterGson,
