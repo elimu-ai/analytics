25d45fd50856333dcb1274c751977ef969a3c768
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-05-29 23:25:13 +0700

    feat: add csv export of letter-sound assessment events
    #128

--- app/src/main/java/ai/elimu/analytics/dao/LetterSoundAssessmentEventDao.kt
+++ app/src/main/java/ai/elimu/analytics/dao/LetterSoundAssessmentEventDao.kt
@@ -3,9 +3,13 @@ package ai.elimu.analytics.dao
 import ai.elimu.analytics.entity.LetterSoundAssessmentEvent
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface LetterSoundAssessmentEventDao {
     @Insert
     fun insert(letterSoundAssessmentEvent: LetterSoundAssessmentEvent)
+
+    @Query("SELECT * FROM LetterSoundAssessmentEvent ORDER BY id")
+    fun loadAll(): List<LetterSoundAssessmentEvent>
 }
