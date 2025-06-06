646691eadb35d883edb211ef0739f962887b9568
Tuan Nguyen <tuancoltech@gmail.com>
2025-06-06 03:33:24 +0000

    Add null safety check for clipboard service access

--- app/src/main/java/ai/elimu/analytics/util/Clipboard.kt
+++ app/src/main/java/ai/elimu/analytics/util/Clipboard.kt
@@ -7,7 +7,8 @@ import android.widget.Toast
 
 object Clipboard {
     fun copy(context: Context, text: String) {
-        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+            ?: return
         clipboard.setPrimaryClip(ClipData.newPlainText(null, text))
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
