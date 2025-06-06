548c83e9f999bc341ef3972e5a4d49fdb7f1da60
Tuan Nguyen <tuancoltech@gmail.com>
2025-06-06 03:00:38 +0000

    Make `Android ID` value copiable.

---
+++ app/src/main/java/ai/elimu/analytics/util/Clipboard.kt
@@ -0,0 +1,14 @@
+package ai.elimu.analytics.util
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+
+object Clipboard {
+    fun copy(context: Context, text: String) {
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.setPrimaryClip(ClipData.newPlainText(null, text))
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+}
