8b73f72e1cd0609a2246b681aeb51b15a6fd32db
Tuan Nguyen <tuancoltech@gmail.com>
2025-06-12 03:10:01 +0000

    Add `VideoLearningEventDao`

---
+++ app/src/main/java/ai/elimu/analytics/dao/VideoLearningEventDao.kt
@@ -0,0 +1,22 @@
+package ai.elimu.analytics.dao
+
+import ai.elimu.analytics.entity.VideoLearningEvent
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface VideoLearningEventDao {
+    @Insert
+    fun insert(videoLearningEvent: VideoLearningEvent)
+
+    @Query(
+        "SELECT * FROM VideoLearningEvent ORDER BY " +
+                "CASE WHEN :isDesc = 1 THEN time END DESC," +
+                "CASE WHEN :isDesc = 0 THEN time END ASC"
+    )
+    fun loadAll(isDesc: Boolean = true): List<VideoLearningEvent>
+
+    @Query("SELECT COUNT(*) FROM VideoLearningEvent")
+    fun getCount(): Int
+}
