a01d67241876fc74d5e000cb4ab0d62dd59c2fbe
Tuan Nguyen <tuancoltech@gmail.com>
2025-06-17 03:45:59 +0000

    Add `NumberLearningEventReceiver`

---
+++ app/src/main/java/ai/elimu/analytics/receiver/NumberLearningEventReceiver.kt
@@ -0,0 +1,62 @@
+package ai.elimu.analytics.receiver
+
+import ai.elimu.analytics.db.RoomDb
+import ai.elimu.analytics.entity.NumberLearningEvent
+import ai.elimu.model.v2.enums.analytics.LearningEventType
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import timber.log.Timber
+import java.util.Calendar
+
+class NumberLearningEventReceiver : BroadcastReceiver() {
+    @SuppressLint("HardwareIds")
+    override fun onReceive(context: Context, intent: Intent) {
+        Timber.i("onReceive")
+
+        val timestamp = Calendar.getInstance()
+        Timber.i("timestamp.getTime(): %s", timestamp.time)
+
+        val androidId =
+            Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        Timber.i("androidId: \"$androidId\"")
+
+        val packageName = intent.getStringExtra("packageName") ?: ""
+        Timber.i("packageName: \"$packageName\"")
+
+        val additionalData = intent.getStringExtra("additionalData")
+        Timber.i("additionalData: ${additionalData}")
+
+        val learningEventTypeAsString = intent.getStringExtra("learningEventType") ?: ""
+        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
+        val learningEventType = LearningEventType.valueOf(
+            learningEventTypeAsString
+        )
+        Timber.i("learningEventType: $learningEventType")
+
+        val numberId = intent.getLongExtra("numberId", 0)
+        Timber.i("numberId: $numberId")
+
+        val number = intent.getLongExtra("number", 0)
+        Timber.i("number: \"$number\"")
+
+        val numberLearningEvent = NumberLearningEvent().apply {
+            this.time = timestamp
+            this.androidId = androidId
+            this.packageName = packageName
+            this.additionalData = additionalData
+            this.learningEventType = learningEventType
+            this.numberId = numberId
+            this.number = number
+        }
+
+        // Store in database
+        val roomDb = RoomDb.getDatabase(context)
+        val numberLearningEventDao = roomDb.numberLearningEventDao()
+        RoomDb.databaseWriteExecutor.execute {
+            numberLearningEventDao.insert(numberLearningEvent)
+        }
+    }
+}
