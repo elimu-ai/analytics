0fca3f31e8150a7eccbe3afc739633bae279ff86
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-22 23:39:51 +0700

    refactor: make key values compatible with csv header names
    #336

--- utils/src/main/java/ai/elimu/analytics/utils/BundleKeys.kt
+++ utils/src/main/java/ai/elimu/analytics/utils/BundleKeys.kt
@@ -17,44 +17,44 @@ object BundleKeys {
     const val KEY_ID = "id"
 
     // LearningEvent/AssessmentEvent
-    const val KEY_ANDROID_ID = "androidId"
-    const val KEY_PACKAGE_NAME = "packageName"
+    const val KEY_ANDROID_ID = "android_id"
+    const val KEY_PACKAGE_NAME = "package_name"
     const val KEY_TIMESTAMP = "timestamp"
-    const val KEY_ADDITIONAL_DATA = "additionalData"
+    const val KEY_ADDITIONAL_DATA = "additional_data"
 
     // LearningEvent
     @Deprecated("Will be replaced by `additionalData`")
-    const val KEY_LEARNING_EVENT_TYPE = "learningEventType"
+    const val KEY_LEARNING_EVENT_TYPE = "learning_event_type"
 
     // AssessmentEvent
-    const val KEY_TIME_SPENT_MS = "timeSpentMs"
-    const val KEY_MASTERY_SCORE = "masteryScore"
+    const val KEY_TIME_SPENT_MS = "time_spent_ms"
+    const val KEY_MASTERY_SCORE = "mastery_score"
 
     // LetterSoundAssessmentEvent/LetterSoundLearningEvent
     @Deprecated("Will be replaced by `letterSoundLetters`")
-    const val KEY_LETTER_SOUND_LETTER_TEXTS = "letterSoundLetterTexts"
+    const val KEY_LETTER_SOUND_LETTER_TEXTS = "letter_sound_letter_texts"
 
     @Deprecated("Will be replaced by `letterSoundSounds`")
-    const val KEY_LETTER_SOUND_SOUND_VALUES_IPA = "letterSoundSoundValuesIpa"
+    const val KEY_LETTER_SOUND_SOUND_VALUES_IPA = "letter_sound_sound_values_ipa"
 
-    const val KEY_LETTER_SOUND_LETTERS = "letterSoundLetters"
-    const val KEY_LETTER_SOUND_SOUNDS = "letterSoundSounds"
-    const val KEY_LETTER_SOUND_ID = "letterSoundId"
+    const val KEY_LETTER_SOUND_LETTERS = "letter_sound_letters"
+    const val KEY_LETTER_SOUND_SOUNDS = "letter_sound_sounds"
+    const val KEY_LETTER_SOUND_ID = "letter_sound_id"
 
     // WordAssessmentEvent/WordLearningEvent
-    const val KEY_WORD_TEXT = "wordText"
-    const val KEY_WORD_ID = "wordId"
+    const val KEY_WORD_TEXT = "word_text"
+    const val KEY_WORD_ID = "word_id"
 
     // NumberAssessmentEvent/NumberLearningEvent
-    const val KEY_NUMBER_VALUE = "numberValue"
-    const val KEY_NUMBER_SYMBOL = "numberSymbol"
-    const val KEY_NUMBER_ID = "numberId"
+    const val KEY_NUMBER_VALUE = "number_value"
+    const val KEY_NUMBER_SYMBOL = "number_symbol"
+    const val KEY_NUMBER_ID = "number_id"
 
     // StoryBookLearningEvent
-    const val KEY_STORYBOOK_TITLE = "storyBookTitle"
-    const val KEY_STORYBOOK_ID = "storyBookId"
+    const val KEY_STORYBOOK_TITLE = "storybook_title"
+    const val KEY_STORYBOOK_ID = "storybook_id"
 
     // VideoLearningEvent
-    const val KEY_VIDEO_TITLE = "videoTitle"
-    const val KEY_VIDEO_ID = "videoId"
+    const val KEY_VIDEO_TITLE = "video_title"
+    const val KEY_VIDEO_ID = "video_id"
 }
