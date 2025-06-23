dcb28909fa184c217b8d8ac2a3eee9c408c4c708
Tuan Nguyen <tuancoltech@gmail.com>
2025-06-23 16:43:35 +0700

    Separate analytics event creation from intent to a common function

--- app/src/main/java/ai/elimu/analytics/receiver/NumberLearningEventReceiver.kt
+++ app/src/main/java/ai/elimu/analytics/receiver/NumberLearningEventReceiver.kt
@@ -1,68 +1,21 @@
 package ai.elimu.analytics.receiver
 
 import ai.elimu.analytics.db.persistEvent
-import ai.elimu.analytics.entity.NumberLearningEvent
-import ai.elimu.analytics.utils.BundleKeys
-import ai.elimu.analytics.utils.research.ExperimentAssignmentHelper
-import ai.elimu.model.v2.enums.analytics.LearningEventType
+import ai.elimu.analytics.entity.AnalyticEventType
+import ai.elimu.analytics.entity.createEventFromIntent
 import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.provider.Settings
 import timber.log.Timber
-import java.util.Calendar
 
 class NumberLearningEventReceiver : BroadcastReceiver() {
     @SuppressLint("HardwareIds")
     override fun onReceive(context: Context, intent: Intent) {
         Timber.i("onReceive")
 
-        val timestamp = Calendar.getInstance()
-        Timber.i("timestamp.time: %s", timestamp.time)
-
-        val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-        Timber.i("androidId: \"$androidId\"")
-
-        val packageName = intent.getStringExtra(BundleKeys.KEY_PACKAGE_NAME) ?: ""
-        Timber.i("packageName: \"$packageName\"")
-
-        val additionalData = intent.getStringExtra(BundleKeys.KEY_ADDITIONAL_DATA)
-        Timber.i("additionalData: ${additionalData}")
-
-        val researchExperiment = ExperimentAssignmentHelper.CURRENT_EXPERIMENT
-        val experimentGroup = ExperimentAssignmentHelper.getExperimentGroup(context)
-        Timber.i("researchExperiment: ${researchExperiment} (${experimentGroup})")
-
-        val learningEventTypeAsString = intent.getStringExtra(BundleKeys.KEY_LEARNING_EVENT_TYPE)
-        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
-        val learningEventType = runCatching {
-            learningEventTypeAsString?.let {
-                LearningEventType.valueOf(it)
-            }
-        }.getOrNull()
-        Timber.i("learningEventType: $learningEventType")
-
-        val numberValue = intent.getIntExtra(BundleKeys.KEY_NUMBER_VALUE, 0)
-        Timber.i("numberValue: \"$numberValue\"")
-
-        val numberSymbol = intent.getStringExtra(BundleKeys.KEY_NUMBER_SYMBOL)
-        Timber.i("numberSymbol: \"$numberSymbol\"")
-
-        val numberId = intent.getLongExtra(BundleKeys.KEY_NUMBER_ID, 0)
-        Timber.i("numberId: $numberId")
-
-        val numberLearningEvent = NumberLearningEvent(numberValue).apply {
-            this.time = timestamp
-            this.androidId = androidId
-            this.packageName = packageName
-            this.additionalData = additionalData
-            this.researchExperiment = researchExperiment
-            this.experimentGroup = experimentGroup
-            this.learningEventType = learningEventType
-            this.numberSymbol = numberSymbol
-            this.numberId = numberId
-        }
+        val numberLearningEvent = AnalyticEventType.NUMBER_LEARNING
+            .createEventFromIntent(context, intent)
 
         // Store in database
         numberLearningEvent.persistEvent(context)
--- app/src/main/java/ai/elimu/analytics/receiver/StoryBookLearningEventReceiver.kt
+++ app/src/main/java/ai/elimu/analytics/receiver/StoryBookLearningEventReceiver.kt
@@ -1,64 +1,19 @@
 package ai.elimu.analytics.receiver
 
 import ai.elimu.analytics.db.persistEvent
-import ai.elimu.analytics.entity.StoryBookLearningEvent
-import ai.elimu.analytics.utils.BundleKeys
-import ai.elimu.analytics.utils.research.ExperimentAssignmentHelper
-import ai.elimu.model.v2.enums.analytics.LearningEventType
+import ai.elimu.analytics.entity.AnalyticEventType
+import ai.elimu.analytics.entity.createEventFromIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.provider.Settings
 import timber.log.Timber
-import java.util.Calendar
 
 class StoryBookLearningEventReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Timber.i("onReceive")
+        Timber.i("onReceive StoryBookLearningEventReceiver new")
 
-        val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-        Timber.i("androidId: \"$androidId\"")
-
-        val packageName = intent.getStringExtra(BundleKeys.KEY_PACKAGE_NAME) ?: ""
-        Timber.i("packageName: \"$packageName\"")
-
-        val timestamp = Calendar.getInstance()
-        Timber.i("timestamp.time: %s", timestamp.time)
-
-        val additionalData = intent.getStringExtra(BundleKeys.KEY_ADDITIONAL_DATA)
-        Timber.i("additionalData: ${additionalData}")
-
-        val learningEventTypeAsString = intent.getStringExtra(BundleKeys.KEY_LEARNING_EVENT_TYPE)
-        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
-        val learningEventType = runCatching {
-            learningEventTypeAsString?.let {
-                LearningEventType.valueOf(it)
-            }
-        }.getOrNull()
-
-        Timber.i("learningEventType: $learningEventType")
-
-        val researchExperiment = ExperimentAssignmentHelper.CURRENT_EXPERIMENT
-        val experimentGroup = ExperimentAssignmentHelper.getExperimentGroup(context)
-        Timber.i("researchExperiment: ${researchExperiment} (${experimentGroup})")
-
-        val storyBookTitle: String = intent.getStringExtra(BundleKeys.KEY_STORYBOOK_TITLE)
-                ?: throw IllegalArgumentException("storyBookTitle must be provided")
-        Timber.i("storyBookTitle: \"$storyBookTitle\"")
-
-        val storyBookId = intent.getLongExtra(BundleKeys.KEY_STORYBOOK_ID, 0)
-        Timber.i("storyBookId: $storyBookId")
-
-        val storyBookLearningEvent = StoryBookLearningEvent()
-        storyBookLearningEvent.androidId = androidId
-        storyBookLearningEvent.packageName = packageName
-        storyBookLearningEvent.time = timestamp
-        storyBookLearningEvent.additionalData = additionalData
-        storyBookLearningEvent.learningEventType = learningEventType
-        storyBookLearningEvent.researchExperiment = researchExperiment
-        storyBookLearningEvent.experimentGroup = experimentGroup
-        storyBookLearningEvent.storyBookTitle = storyBookTitle
-        storyBookLearningEvent.storyBookId = storyBookId
+        val storyBookLearningEvent = AnalyticEventType.STORY_BOOK_LEARNING
+            .createEventFromIntent(context, intent)
 
         // Store in database
         storyBookLearningEvent.persistEvent(context)
