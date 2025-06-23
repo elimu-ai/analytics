53930e88770bbcdef2451111bf07f6e11b055e59
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-23 17:00:09 +0700

    feat: display toast when exception in receiver (video learning events)
    #340

--- app/src/main/java/ai/elimu/analytics/receiver/VideoLearningEventReceiver.kt
+++ app/src/main/java/ai/elimu/analytics/receiver/VideoLearningEventReceiver.kt
@@ -8,6 +8,7 @@ import ai.elimu.model.v2.enums.analytics.LearningEventType
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.provider.Settings
 import timber.log.Timber
 import java.util.Calendar
@@ -16,54 +17,60 @@ class VideoLearningEventReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Timber.i("onReceive")
 
-        val timestamp = Calendar.getInstance()
-        Timber.i("timestamp.time: %s", timestamp.time)
+        try {
+            val timestamp = Calendar.getInstance()
+            Timber.i("timestamp.time: %s", timestamp.time)
 
-        val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-        Timber.i("androidId: \"$androidId\"")
+            val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+            Timber.i("androidId: \"$androidId\"")
 
-        val packageName = intent.getStringExtra(BundleKeys.KEY_PACKAGE_NAME) ?: ""
-        Timber.i("packageName: \"$packageName\"")
+            val packageName = intent.getStringExtra(BundleKeys.KEY_PACKAGE_NAME) ?: ""
+            Timber.i("packageName: \"$packageName\"")
 
-        val additionalData = intent.getStringExtra(BundleKeys.KEY_ADDITIONAL_DATA)
-        Timber.i("additionalData: ${additionalData}")
+            val additionalData = intent.getStringExtra(BundleKeys.KEY_ADDITIONAL_DATA)
+            Timber.i("additionalData: ${additionalData}")
 
-        val learningEventTypeAsString = intent.getStringExtra(BundleKeys.KEY_LEARNING_EVENT_TYPE)
-        Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
-        val learningEventType = runCatching {
-            learningEventTypeAsString?.let {
-                LearningEventType.valueOf(it)
+            val learningEventTypeAsString = intent.getStringExtra(BundleKeys.KEY_LEARNING_EVENT_TYPE)
+            Timber.i("learningEventTypeAsString: \"$learningEventTypeAsString\"")
+            val learningEventType = runCatching {
+                learningEventTypeAsString?.let {
+                    LearningEventType.valueOf(it)
+                }
+            }.getOrNull()
+
+            Timber.i("learningEventType: $learningEventType")
+
+            val researchExperiment = ExperimentAssignmentHelper.CURRENT_EXPERIMENT
+            val experimentGroup = ExperimentAssignmentHelper.getExperimentGroup(context)
+            Timber.i("researchExperiment: ${researchExperiment} (${experimentGroup})")
+
+            val videoTitle = intent.getStringExtra(BundleKeys.KEY_VIDEO_TITLE) ?: ""
+            Timber.i("videoTitle: \"$videoTitle\"")
+
+            val videoId = intent.getLongExtra(BundleKeys.KEY_VIDEO_ID, 0)
+            Timber.i("videoId: $videoId")
+
+            val videoLearningEvent = VideoLearningEvent()
+            videoLearningEvent.time = timestamp
+            videoLearningEvent.androidId = androidId
+            videoLearningEvent.packageName = packageName
+            videoLearningEvent.additionalData = additionalData
+            videoLearningEvent.learningEventType = learningEventType
+            videoLearningEvent.researchExperiment = researchExperiment
+            videoLearningEvent.experimentGroup = experimentGroup
+            videoLearningEvent.videoTitle = videoTitle
+            videoLearningEvent.videoId = videoId
+
+            // Store in database
+            val roomDb = RoomDb.getDatabase(context)
+            val videoLearningEventDao = roomDb.videoLearningEventDao()
+            RoomDb.databaseWriteExecutor.execute {
+                videoLearningEventDao.insert(videoLearningEvent)
             }
-        }.getOrNull()
-
-        Timber.i("learningEventType: $learningEventType")
-
-        val researchExperiment = ExperimentAssignmentHelper.CURRENT_EXPERIMENT
-        val experimentGroup = ExperimentAssignmentHelper.getExperimentGroup(context)
-        Timber.i("researchExperiment: ${researchExperiment} (${experimentGroup})")
-
-        val videoTitle = intent.getStringExtra(BundleKeys.KEY_VIDEO_TITLE) ?: ""
-        Timber.i("videoTitle: \"$videoTitle\"")
-
-        val videoId = intent.getLongExtra(BundleKeys.KEY_VIDEO_ID, 0)
-        Timber.i("videoId: $videoId")
-
-        val videoLearningEvent = VideoLearningEvent()
-        videoLearningEvent.time = timestamp
-        videoLearningEvent.androidId = androidId
-        videoLearningEvent.packageName = packageName
-        videoLearningEvent.additionalData = additionalData
-        videoLearningEvent.learningEventType = learningEventType
-        videoLearningEvent.researchExperiment = researchExperiment
-        videoLearningEvent.experimentGroup = experimentGroup
-        videoLearningEvent.videoTitle = videoTitle
-        videoLearningEvent.videoId = videoId
-
-        // Store in database
-        val roomDb = RoomDb.getDatabase(context)
-        val videoLearningEventDao = roomDb.videoLearningEventDao()
-        RoomDb.databaseWriteExecutor.execute {
-            videoLearningEventDao.insert(videoLearningEvent)
+        } catch (e: Exception) {
+            Timber.e(e)
+            val results: Bundle = getResultExtras(true)
+            results.putString("errorClassName", e::class.simpleName);
         }
     }
 }
