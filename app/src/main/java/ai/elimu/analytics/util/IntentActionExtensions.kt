57c333e184a5ab4b2e816c334085a0d9e3ded72b
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-26 20:08:29 +0700

    refactor: re-order to match other files

--- app/src/main/java/ai/elimu/analytics/util/IntentActionExtensions.kt
+++ app/src/main/java/ai/elimu/analytics/util/IntentActionExtensions.kt
@@ -7,10 +7,15 @@ fun IntentAction.toEventType(): EventType {
     return when (this) {
         IntentAction.LETTER_SOUND_ASSESSMENT -> EventType.LETTER_SOUND_ASSESSMENT
         IntentAction.LETTER_SOUND_LEARNING -> EventType.LETTER_SOUND_LEARNING
+
         IntentAction.WORD_ASSESSMENT -> EventType.WORD_ASSESSMENT
-        IntentAction.NUMBER_LEARNING -> EventType.NUMBER_LEARNING
-        IntentAction.STORYBOOK_LEARNING -> EventType.STORY_BOOK_LEARNING
-        IntentAction.VIDEO_LEARNING -> EventType.VIDEO_LEARNING
         IntentAction.WORD_LEARNING -> EventType.WORD_LEARNING
+
+        IntentAction.NUMBER_LEARNING -> EventType.NUMBER_LEARNING
+        // TODO: number assessment
+
+        IntentAction.STORYBOOK_LEARNING -> EventType.STORY_BOOK_LEARNING
+
+        IntentAction.VIDEO_LEARNING -> EventType.VIDEO_LEARNING
     }
 }
