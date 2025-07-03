71299c21d1755b9731d71a09ed8e64089b96d5b8
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-07-03 16:51:08 +0700

    feat: add sort order enum

---
+++ utils/src/main/java/ai/elimu/analytics/utils/enum/SortOrder.kt
@@ -0,0 +1,9 @@
+package ai.elimu.analytics.utils.enum
+
+/**
+ * Used for ordering data when querying DAOs and Content Providers.
+ */
+enum class SortOrder {
+    ASC, // Ascending
+    DESC // Descending
+}
