63a8e839a3bd5fc9c949cc4544ee5d12c93eebbd
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-07-27 11:37:23 +0700

    config: db v24
    #405

--- app/src/main/java/ai/elimu/analytics/db/RoomDb.java
+++ app/src/main/java/ai/elimu/analytics/db/RoomDb.java
@@ -33,7 +33,7 @@ import ai.elimu.analytics.entity.WordAssessmentEvent;
 import ai.elimu.analytics.entity.WordLearningEvent;
 import timber.log.Timber;
 
-@Database(version = 23, entities = {
+@Database(version = 24, entities = {
         LetterSoundAssessmentEvent.class,
         LetterSoundLearningEvent.class,
 
